d6ed15136779190e55ef5a911970d4e04357b5a3
eliasvn <eliasvn@metropolia.fi>
2026-02-04 14:24:03 +0200

    initial commit

---
+++ tehtavat/viikko1.3.py
@@ -0,0 +1,3 @@
+kanta=float(input("Suorakulmion kanta: "))
+korkeus=float(input("Suorakulmion korkeus: "))
+print("Suorakulmion piiri: ", 2 * kanta + 2 * korkeus, ", Pinta-ala on: ", kanta * korkeus)
