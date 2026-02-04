69e718221797c69123fe5d97798b1a120633c6a0
eliasvn <eliasvn@metropolia.fi>
2026-02-04 14:32:18 +0200

    initial commit

---
+++ tehtavat/viikko1.4.py
@@ -0,0 +1,4 @@
+luku1=int(input("Satunnainen kokonaisluku 1: "))
+luku2=int(input("Satunnainen kokonaisluku 2: "))
+luku3=int(input("Satunnainen kokonaisluku 3: "))
+print("Lukujen summa: ", sum(luku1,luku2,luku3) , ", Lukujen tulo: ", luku1 * luku2 * luku3 , ", Lukujen keskiarvo: ",  )
